73b92da17113a788a51f28b1d9f4f1a673ad9254
Bohdan <Bohdan@DESKTOP-2RH954L>
2022-10-11 07:59:29 +0300

    Add project files.

---
+++ sns-nrdb/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sns_nrdb
+{
+    public class User
+    {
+
+
+    }
+}
